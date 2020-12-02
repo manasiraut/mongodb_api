cd5d8d2b4bf103c868f4055584ce78ef12272e9a
manasiraut <71024058+manasiraut@users.noreply.github.com>
2020-12-02 17:02:58 +0530

    Add files via upload

---
+++ New folder (2)/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/api", async (req, res) => {
+	// mongodb connection
+	const client = await MongoClient.connect(
+		"mongodb+srv://manasi03:Manasi123@newcluster.d1187.mongodb.net/test",
+		{ useUnifiedTopology: true }
+	);
+	// get database
+	const db = client.db("mydb");
+
+	// get data from collection
+	const data = await db.collection("mydata").find().toArray();
+
+	// send response back to client
+	res.send(data);
+});
+
+app.listen(3000, function () {
+	console.log("listening on 3000");
+});
